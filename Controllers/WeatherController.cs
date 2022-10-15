69047891401720585dde9d6a585a6c2af2ade1fc
Davit Movsisyan <davitmovsisyan44@gmail.com>
2022-10-15 21:48:53 +0400

    Add project files.

---
+++ Controllers/WeatherController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherWidget.Services;
+
+namespace WeatherWidget.Controllers
+{
+    [ApiController]
+    [Route("controller")]
+    public class WeatherController : Controller
+    {
+        [HttpGet("Url")]
+        public string GetWeather(string Url)
+        {
+            return ApiUsingService.GetApiContent(Url);
+        }
+    }
+}
---
+++ Services/ApiUsingService.cs
@@ -0,0 +1,15 @@
+﻿namespace WeatherWidget.Services
+{
+    public static class ApiUsingService
+    {
+       static HttpClient client = new HttpClient();
+        
+        public static string GetApiContent(string Url)
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+
+            string result = client.GetAsync(Url).Result.ToString();
+            return result;
+        }
+    }
+}

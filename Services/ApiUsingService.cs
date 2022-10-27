bb1dc4f953b7b1d85693c99ed6cb2b502a917b52
Davit Movsisyan <davitmovsisyan44@gmail.com>
2022-10-27 19:29:00 +0400

    .

--- Services/ApiUsingService.cs
+++ Services/ApiUsingService.cs
@@ -1,4 +1,6 @@
-﻿namespace WeatherWidget.Services
+﻿using System.Text.Json;
+
+namespace WeatherWidget.Services
 {
     public class ApiUsingService
     {
@@ -9,13 +11,12 @@
             _httpClientFactory = httpClientFactory;
         }
 
-        public async Task<string> GetApiContent(int longtidute,int latidute)
+        public async Task<string> GetApiContent(float longtidute,float latidute)
         {
             string Url = $"data/2.5/weather?lat={latidute}&lon={longtidute}&appid=6cd73d4dbb7ae06b0adde9ce51f9e0f0";
 
             var httpClient = _httpClientFactory.CreateClient("WethForc");
-            var httpResponseMessage = await httpClient.GetAsync(Url);
-
+            var httpResponseMessage = await httpClient.GetAsync(Url);         
             return await httpResponseMessage.Content.ReadAsStringAsync();
         }
     }
